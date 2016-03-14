f61d6b956790fbaff4dfd54bfea6295d1179071e
Alexandre Dulaunoy <a@foo.be>
2016-03-14 20:50:51 +0100

    urlarchiver pypi name changed

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name="url_archiver",
+    name="urlarchiver",
     version="0.1",
     packages=find_packages(),
     install_requires=['requests', 'url-normalize'],
